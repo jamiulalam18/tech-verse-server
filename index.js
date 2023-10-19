0ab24113fa759d43f9a199a89f3406d1eb6ad568
jamiulalam18 <jamiulalam18@gmail.com>
2023-10-19 15:01:46 -0400

    DB connected and data get functions

--- index.js
+++ index.js
@@ -9,6 +9,86 @@ app.use(cors());
 app.use(express.json());
 
 
+const uri =
+  "mongodb+srv://jamiulalam95:lrzGV94TqnPVtHWC@cluster0.idds8so.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const dataset = client.db("techVerseDB");
+
+    const usersCollection = dataset.collection("users");
+
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await usersCollection.findOne({ _id: new ObjectId(id) });
+      res.send(result);
+    });
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await usersCollection.deleteOne({ _id: new ObjectId(id) });
+      res.send(result);
+    });
+
+    const productsCollection = dataset.collection("products");
+    app.get("/products", async (req, res) => {
+      const result = await productsCollection.find().toArray();
+      res.send(result);
+    });
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await productsCollection.findOne({
+        _id: new ObjectId(id),
+      });
+      res.send(result);
+    });
+    app.get('/products/brand/:brand', async (req, res) => {
+        const brand = req.params.brand;
+        const result = await productsCollection.find({brand_name:brand}).toArray();
+        res.send(result);
+        
+    });
+
+    const brandsCollection = dataset.collection('brands');
+    app.get("/brandInfo", async (req, res) => {
+        const result = await brandsCollection.find().toArray();
+        res.send(result);
+      });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 app.get("/", (req, res) => {
   res.send("Hello from server!");
 });
