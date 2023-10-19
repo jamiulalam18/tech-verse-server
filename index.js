d909ae35ec2bb48a009c7a3b7d9db1e73866238c
jamiulalam18 <jamiulalam18@gmail.com>
2023-10-19 14:59:48 -0400

    vercel deployment tested

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const port = process.env.PORT || 5000;
-
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 
+//middleware
 app.use(cors());
-
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello from server!');
+
+app.get("/", (req, res) => {
+  res.send("Hello from server!");
 });
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
