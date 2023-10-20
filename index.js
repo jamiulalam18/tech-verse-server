5d1de6237fd83c4b0388be9e96e4f928ad890441
jamiulalam18 <jamiulalam18@gmail.com>
2023-10-20 10:56:30 -0400

    GET functionality created

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
 const uri =
   "mongodb+srv://jamiulalam95:lrzGV94TqnPVtHWC@cluster0.idds8so.mongodb.net/?retryWrites=true&w=majority";
 
@@ -28,6 +27,8 @@ async function run() {
     const dataset = client.db("techVerseDB");
 
     const usersCollection = dataset.collection("users");
+    const brandsCollection = dataset.collection("brands");
+    const productsCollection = dataset.collection("products");
 
     app.get("/users", async (req, res) => {
       const result = await usersCollection.find().toArray();
@@ -52,7 +53,6 @@ async function run() {
       res.send(result);
     });
 
-    const productsCollection = dataset.collection("products");
     app.get("/products", async (req, res) => {
       const result = await productsCollection.find().toArray();
       res.send(result);
@@ -64,18 +64,37 @@ async function run() {
       });
       res.send(result);
     });
-    app.get('/products/brand/:brand', async (req, res) => {
-        const brand = req.params.brand;
-        const result = await productsCollection.find({brand_name:brand}).toArray();
-        res.send(result);
-        
+    app.get("/brandDetails/:id", async (req, res) => {
+      const id = req.params.id;
+      const brand = await brandsCollection.findOne({
+        _id: new ObjectId(id),
+      });
+      const result = await productsCollection
+        .find({ brand_name: brand.brand_name })
+        .toArray();
+      res.send(result);
     });
 
-    const brandsCollection = dataset.collection('brands');
     app.get("/brandInfo", async (req, res) => {
-        const result = await brandsCollection.find().toArray();
-        res.send(result);
+      const result = await brandsCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/brandInfo/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await brandsCollection.findOne({
+        _id: new ObjectId(id),
       });
+      res.send(result);
+    });
+
+    app.get("/brandInfoByName/:name", async (req, res) => {
+      const name = req.params.name;
+      const result = await brandsCollection.findOne({
+        brand_name: name,
+      });
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
