5e843d713f3b428c6ad580b78e0507a574acbbed
jamiulalam18 <jamiulalam18@gmail.com>
2023-10-24 13:00:02 -0400

    cart handling

--- index.js
+++ index.js
@@ -36,20 +36,45 @@ async function run() {
     });
 
     app.get("/users/:id", async (req, res) => {
+      console.log("/users/:id");
       const id = req.params.id;
       const result = await usersCollection.findOne({ _id: new ObjectId(id) });
       res.send(result);
     });
 
     app.get("/usersByEmail/:email", async (req, res) => {
+      console.log("/usersByEmail/:email");
       const email = req.params.email;
       const result = await usersCollection.findOne({ email: email });
       res.send(result);
     });
 
     app.post("/users", async (req, res) => {
+      console.log("/users");
       const user = req.body;
       const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.patch("/usersCart/:id", async (req, res) => {
+      console.log("/usersCart/:id")
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedCart = req.body;
+      console.log(id)
+
+      const product = {
+        $set: {
+          shopping_cart: updatedCart,
+        },
+      };
+
+      const result = await usersCollection.updateOne(
+        filter,
+        product,
+        options
+      );
       res.send(result);
     });
 
@@ -60,16 +85,18 @@ async function run() {
     });
 
     app.get("/products", async (req, res) => {
+      console.log("/products")
       const result = await productsCollection.find().toArray();
       res.send(result);
     });
     app.post("/products", async (req, res) => {
-      const new_product=req.body;
+      const new_product = req.body;
       const result = await productsCollection.insertOne(new_product);
       res.send(result);
     });
 
     app.get("/products/:id", async (req, res) => {
+      console.log("/products/:id")
       const id = req.params.id;
       const result = await productsCollection.findOne({
         _id: new ObjectId(id),
@@ -77,49 +104,85 @@ async function run() {
       res.send(result);
     });
 
-    app.put('/products/:id', async (req, res) => {
+    app.put("/products/:id", async (req, res) => {
       const id = req.params.id;
-      const filter = { _id: new ObjectId(id) }
+      const filter = { _id: new ObjectId(id) };
       const options = { upsert: true };
       const updatedProduct = req.body;
 
       const product = {
-          $set: {
-            brand_name: updatedProduct.brand_name,
-            product_name: updatedProduct.product_name,
-            product_category: updatedProduct.product_category,
-            product_image: updatedProduct.product_image,
-            product_price: updatedProduct.product_price,
-            product_rating: updatedProduct.product_rating,
-            product_short_description: updatedProduct.product_short_description,
-            product_long_description: updatedProduct.product_long_description,
-            product_highlights: updatedProduct.product_highlights
-          }
-      }
+        $set: {
+          brand_name: updatedProduct.brand_name,
+          product_name: updatedProduct.product_name,
+          product_category: updatedProduct.product_category,
+          product_image: updatedProduct.product_image,
+          product_price: updatedProduct.product_price,
+          product_rating: updatedProduct.product_rating,
+          product_short_description: updatedProduct.product_short_description,
+          product_long_description: updatedProduct.product_long_description,
+          product_highlights: updatedProduct.product_highlights,
+        },
+      };
 
-      const result = await productsCollection.updateOne(filter, product, options);
+      const result = await productsCollection.updateOne(
+        filter,
+        product,
+        options
+      );
       res.send(result);
-  })
+    });
 
+    app.patch("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedProduct = req.body;
 
+      const product = {
+        $set: {
+          brand_name: updatedProduct.brand_name,
+          product_name: updatedProduct.product_name,
+          product_category: updatedProduct.product_category,
+          product_image: updatedProduct.product_image,
+          product_price: updatedProduct.product_price,
+          product_rating: updatedProduct.product_rating,
+          product_short_description: updatedProduct.product_short_description,
+          product_long_description: updatedProduct.product_long_description,
+          product_highlights: updatedProduct.product_highlights,
+        },
+      };
+
+      const result = await productsCollection.updateOne(
+        filter,
+        product,
+        options
+      );
+      res.send(result);
+    });
 
     app.get("/brandDetails/:id", async (req, res) => {
+      console.log("/brandDetails/:id")
       const id = req.params.id;
+      console.log(id);
       const brand = await brandsCollection.findOne({
         _id: new ObjectId(id),
       });
       const result = await productsCollection
         .find({ brand_name: brand.brand_name })
         .toArray();
+      console.log(result);
       res.send(result);
     });
 
+
     app.get("/brandInfo", async (req, res) => {
+      console.log("/brandInfo");
       const result = await brandsCollection.find().toArray();
       res.send(result);
     });
 
     app.get("/brandInfo/:id", async (req, res) => {
+      console.log("/brandInfo/:id");
       const id = req.params.id;
       const result = await brandsCollection.findOne({
         _id: new ObjectId(id),
@@ -128,6 +191,7 @@ async function run() {
     });
 
     app.get("/brandInfoByName/:name", async (req, res) => {
+      console.log("/brandInfoByName/:name"); 
       const name = req.params.name;
       const result = await brandsCollection.findOne({
         brand_name: name,
@@ -143,6 +207,7 @@ async function run() {
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
+    
   }
 }
 run().catch(console.dir);
@@ -154,8 +219,6 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
-
-
 
 // ভার্সেল ও ডিপ্লয় করার পর যদি ডাটা আসতে লেট হয় await client.connect() এবং await client.db (এই লাইনটা নিচের দিকে)এই দুইটা লাইন কেটে দিয়ে
 // যে গুলো require করতেছেন সেগুলো এমনভাবে রিফ্যাক্টর করে নেবেন।
